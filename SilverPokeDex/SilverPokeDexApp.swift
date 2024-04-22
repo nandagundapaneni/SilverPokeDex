315b0a68c6fadf5a531bb00a6bc786d4efad53ee
Nanda Gundapaneni <nandagundapaneni@gmail.com>
2024-04-23 01:26:35 +0530

    Initial Commit

---
+++ SilverPokeDex/SilverPokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverPokeDexApp.swift
+//  SilverPokeDex
+//
+//  Created by Nanda Gundapaneni on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SilverPokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
